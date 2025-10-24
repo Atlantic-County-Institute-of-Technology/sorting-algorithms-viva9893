46aba47cb39201151c02e6a18402277997aa2ca8
viva9893 <159032374+viva9893@users.noreply.github.com>
2025-10-24 16:16:01 -0400

    Fix typos and improve user prompts in main.py

--- lastname_main.py
+++ lastname_main.py
@@ -2,14 +2,15 @@
 # 01_bubble_sort.py
 # author: Cesar Vivas
 # last update:
-# import random library for list generation
+
 
 
 def main():
+    
     # start menu
     print("You have entered SORTED-SORTS")
     print("By using this application you have agree to generating random list  to run different sorting algorithms ")
-    print("If at time you wish to not continue press '0', and you will be allow to leave the program")
+    print("If at time you wish not to continue, press '0', and you will be allowed to leave the program")
 
     print("[-] 0. Exit Site\n"
           "[-] 1. generate a list \n")
@@ -23,7 +24,7 @@ def main():
 
 
     try:
-        selection = int(input("Please select an option : "))
+        selection = int(input("Please select an option (0-1): "))
     except ValueError:
         print("(Invalid input")
 
@@ -31,6 +32,7 @@ def main():
         print("Program is existing")
         exit()
     elif selection == 1:
+        # code will start with a preset list of 10
         while True:
 
             import random
@@ -40,13 +42,13 @@ def main():
 
             while True:
                 print("[-] 0. Exit\n"
-                      "[-] 1. Resize list 🍎 \n"
-                      "[-] 2. Sort list \n"
+                      "[-] 1. Resize list\n"
+                      "[-] 2. Sort list \n" 
 
                       )
 
                 try:
-                    selection_0 = int(input("Please select an option(0-2) : "))
+                    selection_0 = int(input("Please select an option(0-4) : "))
                 except ValueError:
                     print("(Invalid input")
                     continue
@@ -55,20 +57,21 @@ def main():
                     print("Program is existing")
                     exit()
 
-                elif selection_0 == 1:
-                #Unser can adjust the perameters for the list
+                elif selection_0 == 1:  # option 1 - resize list by adding indices
+                #User can adjust the parameters for the list
                     print(f"current amount of indices is : {indices}")
                     while True:
                         try:
                             added_indexes = int(input("please indicate a value for how much indexes will be add add : "))
                             if added_indexes >= 0:
                                 indices += added_indexes
-                                break  #end loop if input is vallued
-                            assert added_indexes > 0
+                                break  #end loop if input is valid
+                            assert isinstance(added_indexes,int) and added_indexes > 0 
                         except AssertionError:
-                            print("Number is invalid!")
-                elif selection_0 == 2:
-
+                            print("Value must be an integer an postive, try again")
+                            
+                elif selection_0 == 2: #option 2 - apply a sorting algorithm. 
+                    print("     ")
 
 
 
