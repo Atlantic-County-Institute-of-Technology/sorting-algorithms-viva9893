7b512ea9f770801a36e8afa5b37fda7caaec71cf
viva9893 <159032374+viva9893@users.noreply.github.com>
2025-10-24 19:02:34 -0400

    Refactor bubble_sort function parameter name

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,20 +3,20 @@
 # last update:
 
 # perform the bubblesort
-def bubble_sort(values):
+def bubble_sort(list):
     sorting_actions = 0
-    for i in range(len(values) - 1):
+    for i in range(len(list) - 1):
         # assume the final value in each pass is sorted
-        for j in range(len(values) -i - 1):
+        for j in range(len(list) -i - 1):
 
             # sorting action (SWAP) if consecutive action is less than lower index
             if values[j] > values[j+1]:
                 values[j], values[j+1] = values[j+1], values[j]
 
-# add count to the amount of sorting actions
+# add count to the number of sorting actions
                 sorting_actions +=1
     print(f"sorting actions = {sorting_actions}")
-    return values
+    return list
 # called sorted list
-print(f"Sorted Values = {bubble_sort(values)}")
+# print(f"Sorted Values = {bubble_sort(list)}")
 
