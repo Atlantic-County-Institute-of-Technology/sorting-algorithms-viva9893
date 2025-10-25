81d379d9fab711b0d7b9c8d8d98cfe28824c4d4f
viva9893 <159032374+viva9893@users.noreply.github.com>
2025-10-24 20:42:43 -0400

    final.bubble

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,22 +1,22 @@
 # 01_bubble_sort.py
 # author: Cesar Vivas
-# last update:
+# last update:10.24.25
 
 # perform the bubblesort
-def bubble_sort(list):
+def bubble_sort(values):
     sorting_actions = 0
-    for i in range(len(list) - 1):
+    for i in range(len(values) - 1):
         # assume the final value in each pass is sorted
-        for j in range(len(list) -i - 1):
+        for j in range(len(values) -i - 1):
 
             # sorting action (SWAP) if consecutive action is less than lower index
             if values[j] > values[j+1]:
                 values[j], values[j+1] = values[j+1], values[j]
 
-# add count to the number of sorting actions
+# add count to the amount of sorting actions
                 sorting_actions +=1
     print(f"sorting actions = {sorting_actions}")
-    return list
+    return values
 # called sorted list
-# print(f"Sorted Values = {bubble_sort(list)}")
+# print(f"Sorted Values = {bubble_sort(values)}")
 
